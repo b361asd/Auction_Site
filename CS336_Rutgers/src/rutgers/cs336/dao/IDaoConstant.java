ba67a851010ac7e97f019dfc7a3c262291a3fefe
b361asd <5williamchen@gmail.com>
2019-02-12 21:45:46 -0500

    Restarting 01

--- CS336_Rutgers/src/rutgers/cs336/dao/IDaoConstant.java
+++
@@ -1,9 +0,0 @@
-package rutgers.cs336.dao;
-
-public interface IDaoConstant {
-
-	String DATA_NAME_STATUS = "DataNameStatus";
-	String DATA_NAME_MESSAGE = "DataNameMessage";
-	//
-	String DATA_NAME_USER_TYPE = "DataNameUserType";
-}
