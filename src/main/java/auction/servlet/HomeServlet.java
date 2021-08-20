69e170b9064667ffd993717fc480d882e7d60fae
b361asd <5williamchen@gmail.com>
2021-08-19 23:20:51 -0400

    Remove unused imports. Bump version.

--- src/main/java/auction/servlet/HomeServlet.java
+++ src/main/java/auction/servlet/HomeServlet.java
@@ -8,10 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-// import javax.servlet.ServletException;
-// import javax.servlet.http.HttpServlet;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
 
 /**
  * Dispatch Servlet based on userType:
