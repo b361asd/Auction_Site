23392a4c1ad0130a8ff058196853b026e2a517a8
b361asd <5williamchen@gmail.com>
2021-09-16 13:02:20 -0400

    Use try-with-resources for OfferAlert. Remove unneeded comments.

--- src/main/java/auction/db/OfferAlert.java
+++ src/main/java/auction/db/OfferAlert.java
@@ -21,6 +21,7 @@ public class OfferAlert extends DBBase {
                     "triggerTxt",
                     "description",
                     "generateDate");
+
     private static final int[] colSeq_OfferAlert = {1, 2, 4, 5};
 
     /**
@@ -32,31 +33,22 @@ public class OfferAlert extends DBBase {
     public static void doGenerateNewOfferAlertCriterion(
             String userID, Map<String, String[]> parameters) {
         Map output = new HashMap();
-        //
         String criterionName = getStringFromParamMap("criterionName", parameters);
         if (criterionName.length() == 0) {
             criterionName = "Unnamed";
         }
-        //
         StringBuilder sbTrigger =
                 Offer.formatSQLWithParametersForSearchOrAlert(parameters, userID, false);
         StringBuilder sbDesc = Offer.formatAlertDescription(parameters, userID);
-        //
-        Connection con = null;
-        PreparedStatement preparedStmt = null;
-        try {
-            con = getConnection();
-            //
-            preparedStmt = con.prepareStatement(ISQLConstant.SQL_OFFERALERTCRITERION_INSERT);
-            //
+        try (Connection con = getConnection();
+                PreparedStatement preparedStmt =
+                        con.prepareStatement(ISQLConstant.SQL_OFFERALERTCRITERION_INSERT)) {
             preparedStmt.setString(1, getUUID());
             preparedStmt.setString(2, userID);
             preparedStmt.setString(3, criterionName);
             preparedStmt.setString(4, sbTrigger.toString());
             preparedStmt.setString(5, sbDesc.toString());
-            //
             preparedStmt.execute();
-            //
             output.put(DATA_NAME_STATUS, true);
             output.put(DATA_NAME_MESSAGE, "OK");
         } catch (SQLException e) {
@@ -71,23 +63,7 @@ public class OfferAlert extends DBBase {
                     DATA_NAME_MESSAGE,
                     "ERROR=" + "ClassNotFoundException" + ", SQL_STATE=" + e.getMessage());
             e.printStackTrace();
-        } finally {
-            if (preparedStmt != null) {
-                try {
-                    preparedStmt.close();
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-            }
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-            }
         }
-        //
     }
 
     /**
@@ -102,42 +78,32 @@ public class OfferAlert extends DBBase {
         List lstRows = new ArrayList();
         TableData tableData = new TableData(lstHeader_OfferAlert, lstRows, colSeq_OfferAlert);
         output.put(DATA_NAME_DATA, tableData);
-        //
-        Connection con = null;
-        PreparedStatement preparedStmt = null;
-        try {
-            con = getConnection();
-            //
-            preparedStmt =
-                    con.prepareStatement(
-                            isUser
-                                    ? ISQLConstant.SQL_OFFERALERTCRITERION_SELECT_USER
-                                    : ISQLConstant.SQL_OFFERALERTCRITERION_SELECT_EX_USER);
+        try (Connection con = getConnection();
+                PreparedStatement preparedStmt =
+                        con.prepareStatement(
+                                isUser
+                                        ? ISQLConstant.SQL_OFFERALERTCRITERION_SELECT_USER
+                                        : ISQLConstant.SQL_OFFERALERTCRITERION_SELECT_EX_USER); ) {
             preparedStmt.setString(1, userName);
-            //
-            ResultSet rs = preparedStmt.executeQuery();
-            //
-            while (rs.next()) {
-                Object criterionID = rs.getObject(1);
-                Object buyer = rs.getObject(2);
-                Object criterionName = rs.getObject(3);
-                Object triggerTxt = rs.getObject(4);
-                Object description = rs.getObject(5);
-                Object generateDate = rs.getObject(6);
-                //
-                List currentRow = new LinkedList();
-                lstRows.add(currentRow);
-                //
-                currentRow.add(criterionID);
-                currentRow.add(buyer);
-                currentRow.add(criterionName);
-                currentRow.add(triggerTxt);
-                currentRow.add(description);
-                currentRow.add(generateDate);
+            try (ResultSet rs = preparedStmt.executeQuery()) {
+                while (rs.next()) {
+                    Object criterionID = rs.getObject(1);
+                    Object buyer = rs.getObject(2);
+                    Object criterionName = rs.getObject(3);
+                    Object triggerTxt = rs.getObject(4);
+                    Object description = rs.getObject(5);
+                    Object generateDate = rs.getObject(6);
+                    List currentRow = new LinkedList();
+                    lstRows.add(currentRow);
+                    currentRow.add(criterionID);
+                    currentRow.add(buyer);
+                    currentRow.add(criterionName);
+                    currentRow.add(triggerTxt);
+                    currentRow.add(description);
+                    currentRow.add(generateDate);
+                }
             }
-            //
             output.put(DATA_NAME_DATA, tableData);
-            //
             output.put(DATA_NAME_STATUS, true);
             output.put(DATA_NAME_MESSAGE, "OK");
         } catch (SQLException e) {
@@ -162,23 +128,7 @@ public class OfferAlert extends DBBase {
                             + ", DETAILS: "
                             + exceptionToString(e));
             e.printStackTrace();
-        } finally {
-            if (preparedStmt != null) {
-                try {
-                    preparedStmt.close();
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-            }
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-            }
         }
-        //
         return output;
     }
 
@@ -189,19 +139,12 @@ public class OfferAlert extends DBBase {
      */
     public static void deleteOfferAlert(Map<String, String[]> parameters) {
         Map output = new HashMap();
-        //
         String criterionID = getStringFromParamMap("criterionID", parameters);
-        //
-        Connection con = null;
-        PreparedStatement preparedStmt = null;
-        try {
-            con = getConnection();
-            //
-            preparedStmt = con.prepareStatement(ISQLConstant.SQL_OFFERALERTCRITERION_DELETE);
+        try (Connection con = getConnection();
+                PreparedStatement preparedStmt =
+                        con.prepareStatement(ISQLConstant.SQL_OFFERALERTCRITERION_DELETE)) {
             preparedStmt.setString(1, criterionID);
-            //
             preparedStmt.execute();
-            //
             int count = preparedStmt.getUpdateCount();
             if (count == 1) {
                 output.put(DATA_NAME_STATUS, true);
@@ -234,22 +177,6 @@ public class OfferAlert extends DBBase {
                             + ", "
                             + dumpParamMap(parameters));
             e.printStackTrace();
-        } finally {
-            if (preparedStmt != null) {
-                try {
-                    preparedStmt.close();
-                } catch (Throwable t) {
-                    t.printStackTrace();
-                }
-            }
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (Throwable t) {
-                    t.printStackTrace();
-                }
-            }
         }
-        //
     }
 }
