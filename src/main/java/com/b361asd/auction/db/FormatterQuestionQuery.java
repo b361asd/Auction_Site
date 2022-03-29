a7c8650eb2c690a632290377fc555e9e6f85f569
b361asd <5williamchen@gmail.com>
2022-03-29 17:19:59 -0400

    Move FormatterQuestionQuery tests

--- src/main/java/com/b361asd/auction/db/FormatterQuestionQuery.java
+++ src/main/java/com/b361asd/auction/db/FormatterQuestionQuery.java
@@ -10,12 +10,4 @@ public class FormatterQuestionQuery extends DBBase {
                 .append("answer = ''))");
         return sb;
     }
-
-    public static void main(String[] args) {
-        StringBuilder sb = initQuerySearch();
-        addContainTagsCondition2Cols(sb, "question", "answer", "o");
-        addDatetimeConditionLookBack(sb, "questionDate", 7);
-        addCondition(sb, "o.categoryName", OP_SZ_EQUAL_MULTI_NO_ESCAPE, "'car','motorbike'", null);
-        System.out.println(sb);
-    }
 }
---
+++ src/test/java/FormatterQuestionQueryTest.java
@@ -0,0 +1,20 @@
+import com.b361asd.auction.db.DBBase;
+import com.b361asd.auction.db.FormatterQuestionQuery;
+import org.junit.jupiter.api.Test;
+
+public class FormatterQuestionQueryTest {
+
+    @Test
+    void testFormatQuestionQuery() {
+        StringBuilder sb = FormatterQuestionQuery.initQuerySearch();
+        FormatterQuestionQuery.addContainTagsCondition2Cols(sb, "question", "answer", "o");
+        FormatterQuestionQuery.addDatetimeConditionLookBack(sb, "questionDate", 7);
+        FormatterQuestionQuery.addCondition(
+                sb,
+                "o.categoryName",
+                DBBase.OP_SZ_EQUAL_MULTI_NO_ESCAPE,
+                "'car','motorbike'",
+                null);
+        System.out.println(sb);
+    }
+}
