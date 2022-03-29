46a710d6f665b7815cb0bc2f480bd3c1626f0946
b361asd <5williamchen@gmail.com>
2022-03-29 17:42:37 -0400

    Move User tests

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,58 @@
+import com.b361asd.auction.db.User;
+import com.b361asd.auction.servlet.IConstant;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    @Test
+    void testDoVerifyLogin() {
+        Map map = User.doVerifyLogin("user", "user_pwd");
+        System.out.println(IConstant.DATA_NAME_STATUS + "= " + map.get(IConstant.DATA_NAME_STATUS));
+        System.out.println(
+                IConstant.DATA_NAME_MESSAGE + "= " + map.get(IConstant.DATA_NAME_MESSAGE));
+        System.out.println(
+                IConstant.DATA_NAME_USER_TYPE + "= " + map.get(IConstant.DATA_NAME_USER_TYPE));
+    }
+
+    @Test
+    void testDoAddUser() {
+        Map map =
+                User.doAddUser(
+                        "abc",
+                        "123",
+                        "email",
+                        "fN",
+                        "lN",
+                        "123 St",
+                        "Pearl",
+                        "NJ",
+                        "01010",
+                        "39239033",
+                        3);
+        System.out.println(IConstant.DATA_NAME_STATUS + "= " + map.get(IConstant.DATA_NAME_STATUS));
+        System.out.println(
+                IConstant.DATA_NAME_MESSAGE + "= " + map.get(IConstant.DATA_NAME_MESSAGE));
+        System.out.println(
+                IConstant.DATA_NAME_USER_TYPE + "= " + map.get(IConstant.DATA_NAME_USER_TYPE));
+    }
+
+    @Test
+    void testModifyUser() {
+        Map<String, String[]> parameters = new HashMap<>();
+        parameters.put("username", new String[] {"user"});
+        parameters.put("password", new String[] {"user_pwd"});
+        parameters.put("email", new String[] {"user@buyme.com"});
+        parameters.put("firstname", new String[] {"Real"});
+        parameters.put("lastname", new String[] {"Lnuser"});
+        parameters.put("address", new String[] {"123 Main St., Nowhere Town, NJ 56789"});
+        parameters.put("phone", new String[] {"2365678909"});
+        Map map = User.modifyUser(parameters, 3);
+        System.out.println(IConstant.DATA_NAME_STATUS + "= " + map.get(IConstant.DATA_NAME_STATUS));
+        System.out.println(
+                IConstant.DATA_NAME_MESSAGE + "= " + map.get(IConstant.DATA_NAME_MESSAGE));
+        System.out.println(
+                IConstant.DATA_NAME_USER_TYPE + "= " + map.get(IConstant.DATA_NAME_USER_TYPE));
+    }
+}
